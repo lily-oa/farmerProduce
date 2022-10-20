049ac3e58cb44cc7465498c6ef6c938a8ad94f54
lily <a0976198586@gmail.com>
2022-10-20 22:43:51 +0800

    Update 2022-10-20T14:43:49.822Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,4 +4,23 @@ var url = "https://hexschool.github.io/js-filter-data/data.json";
 var productList = document.querySelector('.show-list');
 var btnGroup = document.querySelector('.btn-group');
 var btnCropsType = document.querySelector('.btn-group a');
+var inputTxt = document.querySelector('.input-txt');
+var inputBtn = document.querySelector('.input-btn');
+var resultName = document.querySelector('.result-name');
+var selectList = document.querySelector('.select-group');
+var tableSortGroup = document.querySelector('.table-title');
+var data;
+var filterData = []; //---- axios撈取資料 ---- //
+
+function getData() {
+  axios.get(url).then(function (response) {
+    data = response.data.filter(function (item) {
+      return item.作物名稱;
+    });
+    console.log(data); // init(); //非同步
+  });
+} // 預設渲染畫面
+
+
+getData();
 //# sourceMappingURL=all.js.map
