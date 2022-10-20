7368f2274c2e2f996443dc2cf0db9eca116224b3
lily <a0976198586@gmail.com>
2022-10-20 08:36:59 +0800

    Update 2022-10-20T00:36:58.288Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,2 +1,7 @@
 "use strict";
+
+var url = "https://hexschool.github.io/js-filter-data/data.json";
+var productList = document.querySelector('.show-list');
+var btnGroup = document.querySelector('.btn-group');
+var btnCropsType = document.querySelector('.btn-group a');
 //# sourceMappingURL=all.js.map
