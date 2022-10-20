45622e57f6117cf6b318035b9bc3f2774665c6c5
lily <a0976198586@gmail.com>
2022-10-20 08:36:41 +0800

    update

---
+++ app/assets/js/all.js
@@ -0,0 +1,5 @@
+const url = "https://hexschool.github.io/js-filter-data/data.json";
+const productList = document.querySelector('.show-list');
+
+const btnGroup = document.querySelector('.btn-group');
+const btnCropsType = document.querySelector('.btn-group a');
