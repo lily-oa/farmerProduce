ff937d815d6a60ddf230886fe4645cc47236679f
lily <a0976198586@gmail.com>
2022-10-21 08:16:15 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,6 +1,5 @@
 const url = "https://hexschool.github.io/js-filter-data/data.json";
 const productList = document.querySelector('.show-list');
-
 const btnGroup = document.querySelector('.btn-group');
 const btnCropsType = document.querySelector('.btn-group a');
 const inputTxt = document.querySelector('.input-txt');
@@ -17,11 +16,29 @@ function getData(){
   axios.get(url)
   .then(function(response){
     data = response.data.filter(item => item.作物名稱);
-    console.log(data);
-    // init(); //非同步
+    // console.log(data);
+    init(); //非同步
   });
 }
 
-// 預設渲染畫面
+// 預設渲染畫面  初始值
+function init(){
+  productList.innerHTML = `
+  <tr class="table-waiting">
+    <td colspan="7">請輸入並搜尋想比價的作物名稱^_^</td>
+  </tr>`;
 
+  inputBtn.addEventListener('click', searchCrops, false);
+}
+
+// 搜尋資料
+function searchCrops(e){
+  if(inputTxt.value.trim() !== ""){
+    inputBtn.classList.add('btn-active');
+  }else{
+    return;
+  }
+
+  filterData = data.filter(item => item.作物名稱.match(inputTxt.value.trim()));
+}
 getData();
