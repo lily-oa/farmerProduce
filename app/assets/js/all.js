f519d2b9e5276861c60f5ea1c90e58ded475315b
lily <a0976198586@gmail.com>
2022-10-23 14:17:51 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,7 +1,7 @@
 const url = "https://hexschool.github.io/js-filter-data/data.json";
 const productList = document.querySelector('.show-list');
 const btnGroup = document.querySelector('.btn-group');
-const btnCropsType = document.querySelector('.btn-group a');
+const btnCropsType = document.querySelectorAll('.btn-group a');  //共三顆按鈕要用"querySelectorAll
 const inputTxt = document.querySelector('.input-txt');
 const inputBtn = document.querySelector('.input-btn');
 const resultName = document.querySelector('.result-name');
@@ -22,7 +22,6 @@ function getData(){
 }
 
 // 預設渲染畫面  初始值
-
 function init(){
   productList.innerHTML = `
   <tr class="table-waiting">
@@ -30,6 +29,8 @@ function init(){
   </tr>`;
 
   inputBtn.addEventListener('click', searchCrops, false);
+  // inputTxt.addEventListener('keyup', searchCropsKey, false );
+
 }
 
 // 搜尋資料
@@ -42,5 +43,25 @@ function searchCrops(e){
 
   // 過濾輸入值的空白
   filterData = data.filter(item => item.作物名稱.match(inputTxt.value.trim()));
+  btnCropsType.forEach(item => item.classList.remove('active'));
+
+  resultName.textContent = inputTxt.value.trim();
+  inputTxt.value = '';
+  inputBtn.classList.remove('btn-active');
+  resetSelect();
+
+  if(filterData.length === 0){
+    let content = `<tr class="table-waiting"><td colspan="7">查詢不到交易資訊</td></tr>`;
+    productList.innerHTML = content;
+    return;
+  };
+  // update(filterData);
 }
 getData();
+
+
+// 清空下拉選單
+function resetSelect() {
+  selectList[0].value = '排序篩選';
+  selectList[1].value = '排序';
+}
